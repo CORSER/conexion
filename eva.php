122bc95274408b4e0abd2edc931abf9986e058ec
corser <magarcia231@misen.edu.co>
2016-06-23 13:19:14 -0500

    andres

---
+++ eva.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+	$documento=$_POST["documento"];
+	$nombre=$_POST["nombre"];
+	$apellidos=$_POST["apellidos"];
+
+	try{
+		$conexion=new PDO ('mysql:host=sql5.freemysqlhosting.net; dbname=sql5124996', 'sql5124996', 'c2d2UyxQzL');
+		$conexion->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$conexion->exec("SET CHARACTER SET utf8");
+		$sql="insert into aprendices (documento, nombres, apellidos) values (:doc, :nom, :ape)";
+		
+
+		$resultado=$conexion->prepare($sql);
+		$resultado->execute(array(":doc"=>$documento, ":nom"=>$nombre, ":ape"=>$apellidos));
+	$sql="SELECT * FROM aprendices";
+		$resultado=$conexion->prepare($sql);
+		$resultado->execute();
+		while ($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
+			
+			echo"documento: " .$registro['documento'] . $registro['nombres'] .$registro['apellidos'] ."<br>";
+		}
+
+	
+	}catch (exception $e){
+		die('error: ' . $e->getmessage());
+	}
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+
+<form action="eva.php" method="post">
+digite su documento:
+<input type="text" name="documento">
+<br>
+digite su nombre:
+<input type="text" name="nombre" size="30">
+<br>
+digite sus apellidos:
+<input type="text" name="apellidos" size="30">
+<br>
+<input type="submit" value="enviar">
+</form>
+
+</body>
+</html>
